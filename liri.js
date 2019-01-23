8d94ebc53edb6f05e0fd70a3233f41eae488d71d
Jack Pearson <gasca.anthony@gmail.com>
2019-01-22 22:43:12 -0700

    uploading Spotify Search function

--- liri.js
+++ liri.js
@@ -33,10 +33,10 @@ switch (liriCommand) {
         concertSearch(userInput);
         break;
     case 'spotify-this-song':
-        spotifySearch(userInput);
+        songSearch(userInput);
         break;
     case 'movie-this':
-        omdbSearch(userInput);
+        filmSearch(userInput);
         break;
     case 'do-what-it-says':
         randomCommand();
@@ -53,34 +53,96 @@ function concertSearch(userInput) {
     axios.get(queryUrl).then(
 
         function(response) {
+
             let concert = response.data[0];
+
             if (response === undefined) {
                 console.log("Unfortunately at this time there are NO upcoming events for this artist. Please search another one!");
             }
             else {
                 console.log("*******NEXT UPCOMING EVENT INFO*******");
+
                 console.log("Venue:" + " " + concert.venue.name);
+
                 console.log("Location:" + " " + concert.venue.city + "," + " " + concert.venue.region);
+
                 let concertDate = moment(response.datetime).format("MM/DD/YYYY");
+
                 console.log("Date:" + " " + concertDate);
+
                 console.log("**************************************");
+
                 let logConcert = "Next Upcoming Event Info :"+"\n" + "Artist: " + concert.lineup[0]+"\n"
                 + "Venue: " + concert.venue.name+"\n" + "Location: " + concert.venue.city + "," + " " + concert.venue.region+"\n" + "Date: " + concertDate+"\n" + "***************"+"\n";
+
                 fs.appendFile("log.txt", logConcert, function(err) {
+
                     if(err) {
                         console.log(err);
                     }
                     else {
-                        console.log("Concert added to the log!");
+                        console.log("Concert info added to the log!");
                     }
                 })
             }
         }
-    ).catch(function (error) {
+    ).catch(function(error) {
         console.log(error);
     });
 }
 
 
+function songSearch(userInput) {
+
+    if (userInput === undefined) {
+
+        userInput = "The Sign Ace of Base";
+        
+        console.log("It looks like you didn't put in a song name. Here's a recommended classic.");
+    
+    }
+    
+
+    spotify.search({type: "track", query: userInput, limit: 1})
+
+        .then(function(response) {
+
+            let songs = response.tracks.items[0];
+
+            let previewUrl;
+
+            console.log("Artist: " + songs.artists[0].name);
+
+            console.log("Song Title: " + songs.name);
+
+            console.log("Album: " + songs.album.name);
+
+            if (songs.preview_url === undefined || songs.preview_url === null) {
+                previewUrl = "Unfortunately there is no preview for this song :( .";
+            }
+            else {
+                previewUrl = songs.preview_url;
+            }
+
+            console.log("Preview Song: " + previewUrl);
+
+            let logSong = "Song Info :"+"\n" + "Artist: " + songs.artists[0].name+"\n"
+            + "Song Title: " + songs.name+"\n" + "Album: " + songs.album.name+"\n" + "Preview Song: " + previewUrl+"\n" + "***************"+"\n";
+
+            fs.appendFile("log.txt", logSong, function(err) {
+
+                if(err) {
+                    console.log(err);
+                }
+                else {
+                    console.log("Song info added to the log");
+                }
+            })
+        })
+        .catch(function(err) {
+            console.log(err);
+        });
+
+}
 
 
